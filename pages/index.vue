0290bc3b97fb99d2a44c6dd970d4122e5df5238f
Daniel Stéphane <daniel-stephane.mbappe@byseven.co>
2023-09-20 17:31:36 +0200

    Setup test seo project

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="productContainer">
+      <div class="product" v-for="(product, i) in data" :key="i">
+        <RouterLink :to="`/catalog/becos_article_${product.id}`">
+          <div>
+            {{  product.title  }}
+          </div>
+        </RouterLink>
+        <div>{{ product.description }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const data = await fetch('https://dummyjson.com/products')
+  .then(res => res.json())
+  .then(data => data.products)
+</script>
